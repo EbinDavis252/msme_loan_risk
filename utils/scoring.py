e086020ede2117f5cc6965d12d6664f898503caa
EbinDavis252 <ebindavis25099@gmail.com>
2025-07-22 13:56:52 +0530

    Update scoring.py

--- utils/scoring.py
+++ utils/scoring.py
@@ -7,19 +7,25 @@ encoders = joblib.load("models/label_encoders.pkl")
 
 def predict_risk(df):
     df_encoded = df.copy()
+    
+    # Drop Risk_Flag if present
+    if 'Risk_Flag' in df_encoded.columns:
+        df_encoded = df_encoded.drop(columns=['Risk_Flag'])
+
+    # Encode categorical features
     categorical_cols = ['Business_Type', 'Existing_Loan', 'Location', 'Owner_Education']
-
-    # Encode using trained encoders
     for col in categorical_cols:
-        if col in df.columns:
-            df_encoded[col] = encoders[col].transform(df[col])
+        if col in df_encoded.columns:
+            df_encoded[col] = encoders[col].transform(df_encoded[col])
 
-    # Prepare features
+    # Drop Business_ID
     features = df_encoded.drop(['Business_ID'], axis=1)
-    predictions = model.predict(features)
-    probabilities = model.predict_proba(features)[:, 1]  # Prob of default
 
-    # Return results
+    # Predict
+    predictions = model.predict(features)
+    probabilities = model.predict_proba(features)[:, 1]
+
+    # Append results
     df['Risk_Prediction'] = predictions
     df['Risk_Probability (%)'] = (probabilities * 100).round(2)
     return df
