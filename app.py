231ccf84573079dbdf08e806b5f688c328a73ef5
EbinDavis252 <ebindavis25099@gmail.com>
2025-07-22 14:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if uploaded_file:
         except Exception as e:
             st.error(f"Prediction error: {e}")
 
-    from utils.visualizations import pie_chart_risk, bar_chart_by_business_type
+    from utils.visualizations  import donut_chart_risk, bar_chart_by_business_type
 
 if 'results_df' in locals():
     st.subheader("📊 Visual Analysis of MSME Risk")
